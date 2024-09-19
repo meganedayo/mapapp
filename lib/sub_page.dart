601ee6296c0c29b687a7501b00d5a60c9884dfd8
akina <e1b23001@oit.ac.jp>
2024-09-19 15:49:04 +0900

    ページ追加ヨ

--- lib/sub_page.dart
+++ lib/sub_page.dart
@@ -5,6 +5,11 @@ class Testpage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('ringo'),
+      ),
+    );
   }
 }
