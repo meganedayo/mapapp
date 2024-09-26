75e4a54150ca1378e2cdf8a9d422f342781b9797
megane <e1b23021@oit.ac.jp>
2024-09-26 18:02:37 +0900

    遊具詳細画面の一部

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mapapp/aaa.dart';
-import 'package:mapapp/abc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +23,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
   final String title;
 
   @override
@@ -48,11 +45,47 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: const Center(
-        child: AWidget()
-      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                return Container(
+                    height: 500,
+                    width: double.infinity,
+                    color: const Color.fromARGB(255, 90, 255, 227),
+                    child: Column(children: <Widget>[
+                      Image.asset('images/恐竜.png'),
+                      ListTile(
+                        leading: Icon(Icons.flutter_dash),
+                        title: Text("遊具説明"),
+                      ),
+                      Text(
+                        '名前：ブランコ',
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.normal,
+                          fontStyle: FontStyle.normal,
+                          letterSpacing: 4.0,
+                        ),
+                      ),
+                      Text(
+                        '竹で作成されたブランコです。大人が使用しても壊れない丈夫な遊具です。',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.normal,
+                          fontStyle: FontStyle.normal,
+                          letterSpacing: 4.0,
+                        ),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.info_outline_rounded),
+                        title: Text("イラスト切替"),
+                        onTap: () {},
+                      ),
+                    ]));
+              });
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
