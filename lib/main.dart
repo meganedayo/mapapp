f9b07d19072580970260e21b6735ecc893dedb1a
megane <e1b23021@oit.ac.jp>
2024-11-14 17:13:42 +0900

    初期画面にマップが「拡大縮小できる」という旨を表示

--- lib/main.dart
+++ lib/main.dart
@@ -32,94 +32,143 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // ガイドが表示されているかどうかを管理する
+  bool _isGuideVisible = true;
+
+  // ガイドをタッチしたときに非表示にする関数
+  void _hideGuide() {
+    setState(
+      () {
+        _isGuideVisible = false;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 142, 254, 146),
-              ),
-              child: Text('設定'),
+    return Stack(
+      children: [
+        Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: Text(widget.title),
+          ),
+          drawer: Drawer(
+            child: ListView(
+              children: <Widget>[
+                const DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 142, 254, 146),
+                  ),
+                  child: Text('設定'),
+                ),
+                ListTile(
+                  onTap: () {
+                    final url = Uri.parse(
+                        'https://sites.google.com/view/hirakataplaypark');
+                    launchUrl(url);
+                  },
+                  title: const Text('プレーパークホームページ'),
+                ),
+                ListTile(
+                  onTap: () {
+                    final url = Uri.parse(
+                        'https://sites.google.com/view/hirakataplaypark/korigaokapp/picturebook');
+                    launchUrl(url);
+                  },
+                  title: const Text('生き物図鑑'),
+                ),
+                const ListTile(
+                  title: Text('管理者設定'),
+                ),
+              ],
             ),
-            ListTile(
-              onTap: () {
-                final url =
-                    Uri.parse('https://sites.google.com/view/hirakataplaypark');
-                launchUrl(url);
-              },
-              title: const Text('プレーパークホームページ'),
-            ),
-            ListTile(
-              onTap: () {
-                final url = Uri.parse(
-                    'https://sites.google.com/view/hirakataplaypark/korigaokapp/picturebook');
-                launchUrl(url);
-              },
-              title: const Text('生き物図鑑'),
-            ),
-            const ListTile(
-              title: Text('管理者設定'),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-              context: context,
-              builder: (BuildContext context) {
-                return Container(
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                builder: (BuildContext context) {
+                  return Container(
                     height: 500,
                     width: double.infinity,
                     color: const Color.fromARGB(255, 90, 255, 227),
                     child: SingleChildScrollView(
-                        child: Column(children: <Widget>[
-                      SizedBox(
-                        height: 200.0,
-                        width: double.infinity,
-                        child: Image.asset('images/dyson.png'),
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(
+                            height: 200.0,
+                            width: double.infinity,
+                            child: Image.asset('images/dyson.png'),
+                          ),
+                          const ListTile(
+                            leading: Icon(Icons.flutter_dash),
+                            title: Text("遊具説明"),
+                          ),
+                          const Text(
+                            '名前：ブランコ',
+                            style: TextStyle(
+                              fontSize: 25.0,
+                              fontWeight: FontWeight.normal,
+                              fontStyle: FontStyle.normal,
+                              letterSpacing: 4.0,
+                            ),
+                          ),
+                          const Text(
+                            '竹で作成されたブランコです。大人が使用しても壊れない丈夫な遊具です。',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.normal,
+                              fontStyle: FontStyle.normal,
+                              letterSpacing: 4.0,
+                            ),
+                          ),
+                          ListTile(
+                            leading: const Icon(Icons.info_outline_rounded),
+                            title: const Text("イラスト切替"),
+                            onTap: () {}, //一旦ステイ！！！！！！！！！
+                          ),
+                        ],
                       ),
-                      const ListTile(
-                        leading: Icon(Icons.flutter_dash),
-                        title: Text("遊具説明"),
+                    ),
+                  );
+                },
+              );
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          body: const Map1(),
+        ),
+        if (_isGuideVisible)
+          GestureDetector(
+            onTap: _hideGuide, // タッチしたらガイドを消す
+            child: Positioned.fill(
+              child: Container(
+                color: Colors.black.withOpacity(0.5), // 半透明の黒
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        color: Colors.white.withOpacity(0.8), // 半透明の白
+                        Icons.pinch,
+                        size: 300,
                       ),
                       const Text(
-                        '名前：ブランコ',
+                        '拡大縮小できます',
                         style: TextStyle(
-                          fontSize: 25.0,
-                          fontWeight: FontWeight.normal,
-                          fontStyle: FontStyle.normal,
-                          letterSpacing: 4.0,
-                        ),
-                      ),
-                      const Text(
-                        '竹で作成されたブランコです。大人が使用しても壊れない丈夫な遊具です。',
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.normal,
-                          fontStyle: FontStyle.normal,
-                          letterSpacing: 4.0,
-                        ),
-                      ),
-                      ListTile(
-                        leading: const Icon(Icons.info_outline_rounded),
-                        title: const Text("イラスト切替"),
-                        onTap: () {}, //一旦ステイ！！！！！！！！！
-                      ),
-                    ])));
-              });
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-      body: const Map1(),
+                            fontSize: 50,
+                            color: Colors.white,
+                            decoration: TextDecoration.none),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+      ],
     );
   }
 }
