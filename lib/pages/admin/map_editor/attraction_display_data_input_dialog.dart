831741db14ae26df3f4287d5c7409a2bf841a8b0
7wataaa <7wataaa@gmail.com>
2024-12-24 05:22:58 +0900

    [refactor] アトラクション表示データ入力ダイアログにバリデーションを追加し、フォームを実装

--- lib/pages/admin/map_editor/attraction_display_data_input_dialog.dart
+++ lib/pages/admin/map_editor/attraction_display_data_input_dialog.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+const maxNameLength = 20;
+const maxDescriptionLength = 100;
+
 class AttractionDisplayDataInputDialog extends StatefulWidget {
   final Function(String, String) _onSubmitted;
   final Function() _onCanceled;
@@ -18,6 +21,8 @@ class AttractionDisplayDataInputDialog extends StatefulWidget {
 
 class _AttractionDisplayDataInputDialogState
     extends State<AttractionDisplayDataInputDialog> {
+  final _formKey = GlobalKey<FormState>();
+
   final _nameController = TextEditingController();
   final _descriptionController = TextEditingController();
 
@@ -32,27 +37,62 @@ class _AttractionDisplayDataInputDialogState
   Widget build(BuildContext context) {
     return AlertDialog(
       title: const Text("新しいアトラクションを追加"),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextField(
-            controller: _nameController,
-            decoration: const InputDecoration(labelText: "名前"),
-            autofocus: true,
-          ),
-          TextField(
-            controller: _descriptionController,
-            decoration: const InputDecoration(labelText: "説明"),
-            maxLines: 2,
-          ),
-        ],
+      content: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              controller: _nameController,
+              decoration: const InputDecoration(labelText: "名前"),
+              autofocus: true,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "名前を入力してください";
+                }
+
+                if (value.length > maxNameLength) {
+                  return "名前は $maxNameLength 文字以内で入力してください";
+                }
+
+                return null;
+              },
+              onFieldSubmitted: (value) {
+                _formKey.currentState?.validate();
+              },
+            ),
+            TextFormField(
+              controller: _descriptionController,
+              decoration: const InputDecoration(labelText: "説明"),
+              maxLines: 2,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "説明を入力してください";
+                }
+
+                if (value.length > maxDescriptionLength) {
+                  return "説明は $maxDescriptionLength 文字以内で入力してください";
+                }
+
+                return null;
+              },
+              onFieldSubmitted: (value) {
+                _formKey.currentState?.validate();
+              },
+            ),
+          ],
+        ),
       ),
       actions: [
         ElevatedButton(
-          onPressed: () => widget._onSubmitted(
-            _nameController.text,
-            _descriptionController.text,
-          ),
+          onPressed: () {
+            if (!_formKey.currentState!.validate()) return;
+
+            widget._onSubmitted(
+              _nameController.text,
+              _descriptionController.text,
+            );
+          },
           style: ElevatedButton.styleFrom(
             backgroundColor: Theme.of(context).primaryColor,
             foregroundColor: Theme.of(context).colorScheme.onPrimary,
